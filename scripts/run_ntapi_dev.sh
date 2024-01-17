920d85864e382748c5764df9926bcc5b8a4cb17b
Anmol Singh <anmol@candybanners.com>
2024-01-17 14:12:13 -0500

    deleted files

--- scripts/run_ntapi_dev.sh
+++
@@ -1,4 +0,0 @@
-# export DATABASE_URL=postgresql://pgtest:pgtest@localhost:5432/pgtest
-# export ALEMBIC_DATABASE_URL=postgresql://pgtest:pgtest@localhost:5432/pgtest
-# export DATABASE_URL=postgresql://pg123:pg123@localhost:5432/pg123
-nt_api
